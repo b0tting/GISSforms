a8dafbb8bc3630d15f42a31500d8e863c4fe5b08
mark_gh@malkav.dds.nl <82Dollars!>
2020-01-29 14:33:42 +0100

    Fixed error in ghetto webhook

--- github_webhook.php
+++ github_webhook.php
@@ -2,7 +2,7 @@
 const GIT_COMMAND = "sudo /usr/bin/git pull 2>&1";
 
 print("GISS ghetto CI/CD to pull changes from github or other code repos<br>");
-$logline = "Received GIT webhook request from : $access {$_SERVER['REMOTE_ADDR']} - git command " . GIT_COMMAND;
+$logline = "Received GIT webhook request from : {$_SERVER['REMOTE_ADDR']} - git command " . GIT_COMMAND;
 print($logline . "<br>");
 openlog("GISSforms", LOG_PID | LOG_PERROR, LOG_LOCAL0);
 $access = date("Y/m/d H:i:s");
